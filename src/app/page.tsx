066c54860fb67923aafe3cce12dd94ae68e7f726
Santyazuara <santyazuara2004@gmail.com>
2026-03-08 17:25:34 -0600

    Remove StatsStrip from the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import HeroSlider from "@/components/hero/HeroSlider";
 import ValueProp from "@/components/sections/ValueProp";
 import SplitWhySection from "@/components/sections/SplitWhySection";
 import QuickCta from "@/components/sections/QuickCta";
-import StatsStrip from "@/components/sections/StatsStrip";
 import PolicyPreview from "@/components/sections/PolicyPreview";
 import VisualSupport from "@/components/sections/VisualSupport";
 import Advantages from "@/components/sections/Advantages";
@@ -12,7 +11,6 @@ export default function HomePage() {
   return (
     <>
       <HeroSlider />
-      <StatsStrip />
       <ValueProp />
       <SplitWhySection />
       <QuickCta />
